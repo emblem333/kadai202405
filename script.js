76d83def5f34f847191c985dcf4b603c622463a9
emblem333 <122144556+emblem333@users.noreply.github.com>
2024-05-26 15:38:28 +0900

    add

---
+++ script.js
@@ -0,0 +1,18 @@
+const PostMemo = () => {
+  const memoTitle = document.getElementById('memo_title').value;
+  const memoBody = document.getElementById('memo_body').value;
+  const imageFile = document.getElementById('image-input').files[0];
+  if(memoBody.trim() !== '') {
+    //空欄ではなかった場合、以下のオブジェクトに文章（画像）の情報を格納する
+    const Post = {
+      title: memoTitle,
+      body: memoBody,
+      image: imageFile ? URL.createObjectURL(imageFile) : null, //（三項）条件演算子
+      timestamp: new Date().getTime()
+    };
+    //関数呼び出し
+    saveMemo(Post)
+    displayTimeline();
+    document.getElementById('memo_title').value = '';
+  }
+}
